ac48ac2f3311c6b39de98dbd7dca8068203bc027
Andy M <andy@andypants.xyz>
2018-09-13 00:02:26 -0700

    completed survey logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,28 @@ module.exports = function(app){
     })
 
     app.post("/api/friends", function(req, res){
+        var bestFriend, compatability;
+        friendsData.forEach(function(friend){
+            if(!bestFriend){
+                bestFriend = friend.name;
+                compatability = compareScores(req.body.scores, friend.scores);
+            }
+            var checkComp = compareScores(req.body.scores, friend.scores);
+            if(checkComp > compatability){
+                bestFriend = friend.name;
+                compatability = checkComp;
+            }
+        })
         friendsData.push(req.body);
+        res.json(bestFriend);
     })
+
+    function compareScores(user, friend){
+        var diff = 0;
+        for(var i = 0; i < user.length; i++){
+            diff += Math.abs(user[i] - friend[i]);
+        }
+        return diff;
+    }
 
 }
